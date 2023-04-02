4c62fbfe7e6eaa160989623390042767230fb668
“SanketBhoyar96” <bhoyar367@gmail.com>
2023-04-02 22:54:12 +0530

    settercommit

---
+++ Setter_InjectionApp/src/main/java/com/soft/App.java
@@ -0,0 +1,25 @@
+package com.soft;
+
+import org.springframework.context.ApplicationContext;
+//import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.soft.beans.WelcomeMassageGenertaor;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+//    	ApplicationContext context = new ClassPathXmlApplicationContext("com/soft/cnfg/applicationContext.xml");
+//    	System.out.println("IOC container Started");
+//    	
+//    	WelcomeMassageGenertaor Genertaor = (WelcomeMassageGenertaor) context.getBean("massageGenrator");
+//    	System.out.println("Massage::"+ Genertaor.massageGenrator("Sanket"));
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/soft/cnfg/applicationContext.xml");
+    	System.out.println("IOC container Started");
+    	WelcomeMassageGenertaor Genertaor = (WelcomeMassageGenertaor) context.getBean("massageGenrator");
+    	System.out.println("Massage::"+ Genertaor.massageGenrator("Sanket"));
+    	
+    }
+}
